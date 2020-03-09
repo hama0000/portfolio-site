195e64dd84cc92123a2bff158119a6e3dba57569
hama0000 <hirokih3036@gmail.com>
2020-03-10 02:02:43 +0900

    サイドメニューのスタイル変更、サイドメニューの画面遷移アクション追加、各作品での使用言語が一目で分かるタグを追加

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,14 @@
 $(function () {
+
+    const $li = $('li');
+
+    $li.on('click', function (e) {
+        e.preventDefault();
+
+        $('.panels').stop(true).fadeOut();
+
+        let href = $(this).find('a').attr('href');
+        $(href).stop(true).fadeIn();
+    });
+
 });
